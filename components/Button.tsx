7a6c543095f8e0fe9f72d76f1f7f498d1a89c483
Joshua Oloton <olotonjoshua@gmail.com>
2025-12-21 19:41:46 +0100

    chore: effect navbar responsiveness

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,12 +3,14 @@
 type Props = { 
   classname?: string, 
   onclick?: () => void, 
+  size?: "small" | "regular",
   children: React.ReactNode
  };
 
-const Button = ({ classname, onclick, children }: Props) => {
+const Button = ({ classname, onclick, size, children }: Props) => {
+  const sizeClasses = size === "small" ? "px-6 py-2" : "px-14 py-3";
   return (
-    <button type="button" onClick={onclick} className={`bg-blue400 font-semibold px-14 py-3 cursor-pointer text-white hover:bg-blue400/95 active:bg-blue400/95 active:translate-y-0.5 duration-100 ${classname}`}>
+    <button type="button" onClick={onclick} className={`bg-blue400 font-semibold ${sizeClasses} cursor-pointer text-white hover:bg-blue400/95 active:bg-blue400/95 active:translate-y-0.5 duration-100 ${classname}`}>
       {children}
     </button>
   )
--- components/Header.tsx
+++ components/Header.tsx
@@ -8,12 +8,19 @@ const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <header>
-      <div className="flex justify-between py-6 w-5/6 m-auto">
+      <div className="flex justify-between md:justify-start md:gap-8 items-center py-6 w-5/6 max-w-7xl m-auto">
         <img className="w-24 h-7" src="/assets/logo.svg" alt="Logo" />
         {/* Desktop Nav */}
-        <ul></ul>
+        <ul className="hidden md:flex items-center gap-6 flex-1">
+          <li className="nav-link">Features</li>
+          <li className="nav-link">Pricing</li>
+          <li className="nav-link mr-auto">Resources</li>
+          <li className="nav-link">Login</li>
+          <Button classname="rounded-3xl text-sm" size="small">Sign Up</Button>
+        </ul>
         {/* Toggle icon */}
-        <div onClick={() => setIsOpen(state => !state)}>
+        <div className="md:hidden"
+          onClick={() => setIsOpen(state => !state)}>
           {isOpen ? (
             <X className="text-gray400 cursor-pointer" />
           ): (
@@ -22,7 +29,7 @@ const Header = () => {
         </div>
       </div>
       {/* Mobile Nav */}
-      <div className={`fixed text-white w-full overflow-hidden transition-all duration-300 ease ${isOpen ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"}`}>
+      <div className={`fixed text-white w-full overflow-hidden transition-all duration-300 ease md:hidden ${isOpen ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"}`}>
         <ul className="flex flex-col items-center space-y-5 py-8 w-4/5 m-auto bg-purple950 rounded-xl">
           <li>Features</li>
           <li>Pricing</li>
