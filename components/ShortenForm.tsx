0dc413062c2dde23a11ed46fd993c94e46dc9224
Joshua Oloton <olotonjoshua@gmail.com>
2025-12-24 14:28:32 +0100

    fix: resolve mobile view form ui

--- components/ShortenForm.tsx
+++ components/ShortenForm.tsx
@@ -72,18 +72,18 @@ const ShortenForm = () => {
     <section className="mt-5 bg-[#F0F1F7] pb-4 mb-0">
       <div className="w-5/6 m-auto max-w-5xl relative -top-20">
         <div className="bg-purple950 p-5 md:px-10 rounded-lg shadow-md md:h-40 flex items-center">
-          <form className="flex-1 flex flex-col md:flex-row gap-y-2 md:gap-y-5 items-center md:items-start">
-            <div className="flex flex-col flex-1 mr-0 md:mr-4 w-full">
+          <form className="flex-1 flex flex-col md:flex-row gap-y-2 md:gap-y-5 items-start">
+            <div className="flex flex-col flex-1 mr-0 md:mr-4 w-full min-w-0">
               <input
                 type="text"
                 value={input}
                 onChange={e => setInput(e.target.value)}
                 placeholder="Shorten a link here..."
-                className={`bg-white text-gray500 p-3 rounded-lg ${error ? "border-red400 border-4 placeholder:text-red400/40" : "border-none"} outline-none mb-2 font-semibold`}
+                className={`bg-white text-gray500 p-3 rounded-lg ${error ? "border-red400 border-4 placeholder:text-red400/40" : "border-none"} outline-none mb-2 font-semibold w-full min-w-0`}
               />
               <span className="text-red400 text-sm text-left font-medium italic">{error}</span>
             </div>
-            <Button classname="rounded-lg w-full md:w-auto" onclick={() => shortenLink(input)}>Shorten it!</Button>
+            <Button classname="rounded-lg w-full md:w-auto shrink-0" onclick={() => shortenLink(input)}>Shorten it!</Button>
           </form>
         </div>
         <div className="flex flex-col gap-4 mt-4">
